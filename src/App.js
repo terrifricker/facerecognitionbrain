7b62031171b8187e70de8b5ede1791f7d6ff0dc8
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-24 21:35:13 -0700

    add .js to component imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import Rank from './components/Rank/Rank'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
+import Rank from './components/Rank/Rank.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 
 function App() {
   return (
