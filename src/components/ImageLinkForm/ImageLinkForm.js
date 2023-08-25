c9ec4e90b4cd7cf176f3be8c38bbda3c2a78e574
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-25 13:00:26 -0700

    add instructions above image input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,6 +2,9 @@
 function ImageLinkForm() {
     return (
         <>
+            <h1>Magic Face Detection</h1>
+            <p className="instructions">Enter the url for an image and 
+                the Magic Brain will detect faces.  Give it a try!</p>
             <input type="text" placeholder="Enter url for image" />
             <button>Detect</button>
         </>
