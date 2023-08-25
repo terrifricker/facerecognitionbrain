839194a7ef23f5cb0c2dfac47bf5de99a1641b5c
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-24 22:05:20 -0700

    create Logo component with simple logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import logo from './favicon-frickersolutions.png'
+
+const Logo = () => {
+    return (
+       <>
+            <img src={logo}
+                alt="Fricker Solutions Logo"
+            />
+       </>
+    )
+}
+
+export default Logo;
