a353e9e07fc4ccfeff818c6e9e325afdb378e707
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-24 22:30:07 -0700

    add simple ImageLinkForm component

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,11 @@
+
+function ImageLinkForm() {
+    return (
+        <>
+            <input type="text" placeholder="Enter url for image" />
+            <button>Detect</button>
+        </>
+    )
+}
+
+export default ImageLinkForm;
