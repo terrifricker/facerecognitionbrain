6ab8e9df473b1821a0ea55157761bf80c9a6583b
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-24 22:34:23 -0700

    add simple FaceRecognition component

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import './FaceRecognition.css'
+
+function FaceRecognition() {
+    return (
+        <>
+            <div className="face-recognition-box"></div>
+        </>
+    )
+}
+
+export default FaceRecognition;
