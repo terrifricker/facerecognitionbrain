2393cbc60f2ee298158b8b6ca169591f59982964
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-30 12:30:10 -0700

    add Clarifai http request call to handleClick() function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
@@ -7,13 +6,43 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 
 function App() {
-  // const [imageUrl, setImageUrl] = useState('');
 
   function handleOnChange(event) {
     console.log(event.target.value)
   }
+
   function handleClick(event) {
     console.log('clicked', event.target)
+    // Clarifai constants
+    const PAT = '73efbfec471a4b578501aff88a982de8';
+    const USER_ID = 'clarifai';       
+    const APP_ID = 'main';
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+    // create http request body
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          { "data": { "image": { "url": IMAGE_URL }}}
+      ]
+    });
+    // create http request options
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
   }
 
   return (
