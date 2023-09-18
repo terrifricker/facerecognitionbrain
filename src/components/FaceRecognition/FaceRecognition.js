87d89dbe222743e1d01210240698d9cf05f6e5a8
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-09-18 08:40:00 -0700

    add static image address to show in FaceRecognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,7 +3,9 @@ import './FaceRecognition.css'
 function FaceRecognition() {
     return (
         <>
-            <div className="face-recognition-box"></div>
+            <div className="face-recognition-box">
+                <img src='https://samples.clarifai.com/metro-north.jpg' alt="faces"></img>
+            </div>
         </>
     )
 }
