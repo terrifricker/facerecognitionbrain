21c746d455000b38652fa6f0a05f64497e16c900
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-09-21 10:53:22 -0700

    add calculating bounding box functionality to FaceBox component

--- src/components/FaceBox/FaceBox.js
+++ src/components/FaceBox/FaceBox.js
@@ -1,11 +1,18 @@
 import './FaceBox.css'
 
 
-export default function FaceBox( { box }) {
+export default function FaceBox( { box, width, height }) {
+
+    const top = box.top_row * height
+    const left = box.left_col * width
+    const bottom = box.bottom_row * height
+    const right = box.right_col * width
 
     return (
         <>
-            <div className="bounding-box"></div>
+            <div className="bounding-box" 
+                style={{top: top, left: left, bottom: bottom, right: right}}>
+            </div>
         </>
     )
 }
