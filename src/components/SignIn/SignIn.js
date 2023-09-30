eaca6967daa31784e40510ee512d5a4f1dbe0c7e
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-09-30 13:35:47 -0700

    add onChange functions to SignIn component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,15 +5,29 @@ export default function SignIn({handleSignIn, handleNeedToRegister}) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    function handleOnEmailChange(event) {
+        setEmail(event.target.value)
+    }
+    function handleOnPasswordChange(event) {
+        setPassword(event.target.value)
+    }
     return (
         <>
             <form className="sign-in-form">
                 <h2>Sign In</h2>
                 <label>
-                    Email <input type="email" name="email"/>
+                    Email <input 
+                        type="email" 
+                        name="email"
+                        onChange={handleOnEmailChange}
+                    />
                 </label>
                 <label>
-                    Password <input type="password" name="password"/>
+                    Password <input 
+                        type="password" 
+                        name="password"
+                        onChange={handleOnPasswordChange}
+                    />
                 </label>
                 <button 
                     type="submit"
