9821f807b4df0e564ed952084bb1884583ffad8b
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-10-01 07:43:26 -0700

    write and attach handleOnChange functions for Registration component

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -5,18 +5,40 @@ export default function Registration({handleRegister}) {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    function handleOnNameChange(event) {
+        setName(event.target.value)
+    }
+    function handleOnEmailChange(event) {
+        setEmail(event.target.value)
+    }
+    function handleOnPasswordChange(event) {
+        setPassword(event.target.value)
+    }
     return (
         <>
             <form className="registration-form">
                 <h2>Register</h2>
                 <label>
-                    Name <input name="name"/>
+                    Name <input 
+                            type="text"
+                            name="name"
+                            onChange={handleOnNameChange}
+                    />
                 </label>
                 <label>
-                    Email <input type="email" name="email"/>
+                    Email <input
+                            type="email"
+                            name="email"
+                            onChange={handleOnEmailChange}
+                    />
                 </label>
                 <label>
-                    Password <input type="password" name="password"/>
+                    Password <input 
+                            type="password"
+                            name="password"
+                            onChange={handleOnPasswordChange}
+                    />
                 </label>
                 <button 
                     type="submit"
