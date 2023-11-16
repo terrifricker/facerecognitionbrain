a39a80b66f1ee3cd58eadee060b559b82fa9a985
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-11-16 09:54:57 -0800

    add an image url for user to copy and paste in ImageLinkForm component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,14 @@
-
+import './ImageLinkForm.css'
 export default function ImageLinkForm({ handleOnChange, handleClick }) {
 
     return (
         <>
             <h1>Magic Face Detection</h1>
             <p className="instructions">Enter the url for an image and 
-                the Magic Brain will detect faces.  Give it a try!</p>
+                the Magic Brain will detect faces.  Give it a try!  Use 'copy 
+                image address' when right-clicking an image on the web, or copy 
+                and paste the url below.</p>
+            <p className="instructions">https://assets.codepen.io/4044405/christina-wocintechchat-unsplash.jpg</p>
             <input
                 onChange={handleOnChange}
                 type="text" 
